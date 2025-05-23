d17b2ecd5331e7dd92f0111062a19a6cd60f31d6
eun-seong <eunseong.dev@gmail.com>
2025-05-24 01:06:23 +0900

    feat: 채팅 로깅

--- src/main/java/com/example/gyeongju/chat/model/ChatMessage.java
+++ src/main/java/com/example/gyeongju/chat/model/ChatMessage.java
@@ -3,10 +3,12 @@ package com.example.gyeongju.chat.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ChatMessage {
 
     private String senderId;
